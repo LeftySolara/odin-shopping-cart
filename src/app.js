62ce3bb4d3f7cbb55ab22767e9472815f59cd9dc
Jalen Adams <jalen@jalenkadams.me>
2020-11-14 13:26:05 -0500

    Add: state for keeping track of cart

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/nav';
 import Home from './components/home';
@@ -7,6 +7,8 @@ import Cart from './components/cart';
 import './app.css';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <Router>
       <div className="app">
@@ -14,7 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/shop" component={Shop} />
-          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/cart" render={() => <Cart items={cart} />} />
         </Switch>
       </div>
     </Router>
--- src/components/cart.js
+++ src/components/cart.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './cart.css';
 
-function Cart() {
+function CartIsEmpty() {
   return (
     <div>
+      <h2>Your cart is empty!</h2>
+    </div>
+  );
+}
+
+function Cart(props) {
+  const { items } = props;
+
+  if (items.length === 0) {
+    return <CartIsEmpty />;
+  }
+
+  return (
+    <div id="cart">
       <h2>Cart Page</h2>
     </div>
   );
